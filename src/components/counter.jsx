0dfdaaca2abe4c4c696e9a4719f532c8f2496631
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-09-22 19:14:55 +0300

    Removed local state from counter, rely only on counters' local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 
 class Counter extends React.Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -31,12 +23,12 @@ class Counter extends React.Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
