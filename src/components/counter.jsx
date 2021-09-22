b98a808fc01513b342e6afe8bb8d31a4d97fcb46
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-09-22 16:01:01 +0300

    Mapped array to list of elements

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
 class Counter extends React.Component {
   state = {
     count: 2,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
@@ -10,6 +11,11 @@ class Counter extends React.Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
